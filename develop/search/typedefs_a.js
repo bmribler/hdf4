6bade0c4f1ea18a737cbee884218c76cd63365c0
bmribler <bmribler@users.noreply.github.com>
2024-02-23 01:30:46 +0000

    deploy: f1e2f185ce3ded09695c868e81cd1809417771d4

--- develop/search/typedefs_a.js
+++ develop/search/typedefs_a.js
@@ -1,8 +1,6 @@
 var searchData=
 [
-  ['uchar8_0',['uchar8',['../hdfi_8h.html#afc14fa8aaf9acf8c92f2c6f5a99c589d',1,'hdfi.h']]],
-  ['uint16_1',['uint16',['../hdfi_8h.html#ac2a9e79eb120216f855626495b7bd18a',1,'hdfi.h']]],
-  ['uint32_2',['uint32',['../hdfi_8h.html#acbd4acd0d29e2d6c43104827f77d9cd2',1,'hdfi.h']]],
-  ['uint8_3',['uint8',['../hdfi_8h.html#a33a5e996e7a90acefb8b1c0bea47e365',1,'hdfi.h']]],
-  ['uintn_4',['uintn',['../hdfi_8h.html#a8f0189616483670e42751f89a02501b5',1,'hdfi.h']]]
+  ['void_0',['VOID',['../hdf_8h.html#a7927e087749615dae3114cc27b91c86d',1,'hdf.h']]],
+  ['voidp_1',['VOIDP',['../hdf_8h.html#ab88227ecb274485a440c4020114cc464',1,'hdf.h']]],
+  ['vsubgroup_2',['VSUBGROUP',['../vg__priv_8h.html#a7fdf12d76f7b2cd45ad426afaa5aa740',1,'vg_priv.h']]]
 ];
